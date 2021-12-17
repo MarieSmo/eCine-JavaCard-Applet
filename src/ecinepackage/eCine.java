b667c4bb259ea6f98da720dabdb371559e160bdc
MarieSmo <mariesmo@orange.fr>
2021-12-17 16:23:39 +0100

    set some constants

--- src/ecinepackage/eCine.java
+++ src/ecinepackage/eCine.java
@@ -6,13 +6,21 @@ import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
 
 public class eCine extends Applet {
-/* Constantes */
-public static final byte CLA_MONAPPLET = (byte) 0xB0;
- 
-public static final byte INS_INCREMENTER_COMPTEUR = 0x00;
-public static final byte INS_DECREMENTER_COMPTEUR = 0x01;
-public static final byte INS_INTERROGER_COMPTEUR = 0x02;
-public static final byte INS_INITIALISER_COMPTEUR = 0x03;
+	public static final byte CLA_ECINE = (byte) 0x25;
+	 
+	/* Instructions */
+	public static final byte INS_BUY_TICKET = 0x00;
+	public static final byte INS_GET_BALANCE = 0x01;
+	public static final byte INS_REFUND_BALANCE = 0x02;
+	public static final byte INS_UNLOCK_CARD = 0x03;
+	public static final byte INS_ARCHIVE_TICKET = 0x04;
+	
+	/* Error return value */
+	public static final byte SW1_OK = 0x0A;
+	public static final byte SW2_PURCHASE_OK = 0x01;
+	public static final byte SW1_ERROR = 0x0E;
+	public static final byte SW2_INSUFFICIENT_BALANCE = 0x01;
+	public static final byte SW2_TICKET_MAX_AMOUNT_REACHED = 0x02;
 	
 	private eCine() {
 	}
